fe61a419f0049d799fdb2e5fefaa1c25e315c72f
Patrick Heneise <patrick@zentered.co>
2023-12-25 18:01:47 -0500

    fix: switch arguments to use default folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { readFile } from 'node:fs/promises'
 
-export default async function readFixtures(folder = 'fixtures', name) {
+export default async function readFixtures(name, folder = 'fixtures') {
   const rawFile = await readFile(
     join(process.cwd(), 'test', folder, name),
     'utf8'
