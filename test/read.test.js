7c4605dcdcb405577a612d57896b035f10ba052c
Patrick Heneise <patrick@zentered.co>
2023-12-25 18:03:38 -0500

    test: adjust to function

--- test/read.test.js
+++ test/read.test.js
@@ -2,8 +2,14 @@ import { test } from 'node:test'
 import assert from 'node:assert/strict'
 import readFixtures from '../index.js'
 
-test('readFixtures', async () => {
+test('readFixtures with default folder', async () => {
   const expected = { hello: 'world' }
-  const actual = await readFixtures('fixtures', 'file.json')
+  const actual = await readFixtures('file.json')
+  assert.deepEqual(actual, expected)
+})
+
+test('readFixtures with custom folder', async () => {
+  const expected = { hello: 'world' }
+  const actual = await readFixtures('file.json', 'custom')
   assert.deepEqual(actual, expected)
 })
